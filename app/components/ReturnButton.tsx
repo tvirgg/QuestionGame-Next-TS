87d8bca326b12cf6bbd1df4d34893638e95be766
tvirgg <tvirgg03@gmail.com>
2024-11-01 16:27:18 +0300

    hello

--- app/components/ReturnButton.tsx
+++ app/components/ReturnButton.tsx
@@ -2,20 +2,19 @@
 "use client";
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { FaHome } from 'react-icons/fa';
 
 const ReturnButton: React.FC = () => {
     const router = useRouter();
 
     return (
         <button
-            className="flex justify-center items-center w-[90px] h-[90px] cursor-pointer transition-colors duration-200"
+            className="flex justify-center items-center w-[80px] h-[80px] cursor-pointer transition-colors duration-200"
             onClick={() => router.push('/hello')}
         >
                 <img
                         src="/House.svg"
                         alt="House icon"
-                        className="h-12 w-12 cursor-pointer w-[100px] h-[100px]"
+                        className="h-12 w-12 cursor-pointer w-[80px] h-[80px]"
                     />
         </button>
     );
